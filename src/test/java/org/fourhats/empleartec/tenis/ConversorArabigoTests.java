58609c1532d3d5c04803c67bfdab1f840706eea3
Oscar Marciulionis <moscar.1@netzero.net>
2016-08-08 00:20:56 -0400

    Modification and New File
    Add lines to ConversorArabigoTests.java on 08/06/2016
    New File StringCalculatorTests.java

--- src/test/java/org/fourhats/empleartec/tenis/ConversorArabigoTests.java
+++ src/test/java/org/fourhats/empleartec/tenis/ConversorArabigoTests.java
@@ -20,6 +20,22 @@ public class ConversorArabigoTests {
         chequear(8, "VIII");
         chequear(9, "IX");
         chequear(10, "X");
+        chequear(11, "XI");
+        chequear(12, "XII");
+        chequear(13, "XIII");
+        chequear(14, "XIV");
+        chequear(15, "XV");
+        chequear(16, "XVI");
+        chequear(17, "XVII");
+        chequear(18, "XVIII");
+        chequear(19, "XIX");
+        chequear(20, "XX");
+        chequear(24, "XXIV");
+        chequear(29, "XXIX");
+        chequear(30, "XXX");
+        chequear(34, "XXXIV");
+        chequear(40, "XL");
+        chequear(50, "L");
     }
 
     public void chequear(int numero, String esperado) {
